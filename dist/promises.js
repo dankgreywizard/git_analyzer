c8be3440320dfac1ecc361f6cae3b1a2f0e2a962
Repo Maintainer <dankgreywizard@gmail.com>
2025-12-09 18:21:23 -0500

    chore: stop tracking dist; it's in .gitignore

--- dist/promises.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.writePromise = exports.endPromise = void 0;
-const node_http_1 = require("node:http");
-const node_util_1 = require("node:util");
-exports.endPromise = (0, node_util_1.promisify)(node_http_1.ServerResponse.prototype.end);
-exports.writePromise = (0, node_util_1.promisify)(node_http_1.ServerResponse.prototype.write);
-//# sourceMappingURL=promises.js.map
